35fc3bf70c4ee2dc49572677419ed59823042321
hnaub0709 <ubilayi14@gmail.com>
2022-12-08 15:17:02 +0100

    added neccessary files

--- todoProject/settings.py
+++ todoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)9sc+^wmh*up)i8yz#b8ec6&nhkkt(+%0#(m)ou3_n^852vm9i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
